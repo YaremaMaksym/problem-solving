b74674d3a75014e686684a1ae52933dd9d7e3c46
Maksym Yarema <macsym2@gmail.com>
2023-08-08 19:23:18 +0300

    leetcode: 2. Add Two Numbers

---
+++ src/main/java/org/xsakon/leetcode/p2AddTwoNumbers.java
@@ -0,0 +1,50 @@
+package org.xsakon.leetcode;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+
+public class p2AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode tail = head;
+        head.next = tail;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int num1 = (l1 != null) ? l1.val : 0;
+            int num2 = (l2 != null) ? l2.val : 0;
+
+            int sum = num1 + num2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            ListNode newNode = new ListNode(digit);
+            tail.next = newNode;
+            tail = newNode;
+
+
+
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+
+        return head.next;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
