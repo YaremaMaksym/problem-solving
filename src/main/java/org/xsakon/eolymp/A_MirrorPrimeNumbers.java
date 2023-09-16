311431354aa6dfe0ca76fcd293fea81b6a936e7e
Maksym Yarema <macsym2@gmail.com>
2023-09-16 14:27:44 +0300

    fix A task

--- src/main/java/org/xsakon/eolymp/A_MirrorPrimeNumbers.java
+++ src/main/java/org/xsakon/eolymp/A_MirrorPrimeNumbers.java
@@ -23,7 +23,7 @@ public class A_MirrorPrimeNumbers {
 
     static boolean isPrime(int n) {
 
-        if (n == 1) return true;
+        if (n <= 1) return false;
 
         for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) return false;
