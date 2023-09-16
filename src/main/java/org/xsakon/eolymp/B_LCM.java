11e78db2fd437afcee61e4875b579f2d99bdd4e1
Maksym Yarema <macsym2@gmail.com>
2023-09-16 18:38:35 +0300

    Complete LCM task

---
+++ src/main/java/org/xsakon/eolymp/B_LCM.java
@@ -0,0 +1,43 @@
+package org.xsakon.eolymp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_LCM {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(in.readLine());
+        int[] arr = new int[size];
+        String[] s = in.readLine().split(" ");
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = Integer.parseInt(s[i]);
+        }
+
+        System.out.println(lcmOfMultipleNumbers(arr));
+    }
+
+    static int lcmOfMultipleNumbers(int[] arr) {
+        int lcm = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            lcm = lcm(lcm, arr[i]);
+        }
+        return lcm;
+    }
+
+    static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    static int gcd(int a, int b) {
+        while (a != b) {
+            if (a > b) a -=b;
+            else b -=a;
+        }
+
+        return a;
+    }
+}
