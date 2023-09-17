e2dccb2a16fcc175ef05c70c47117d565d26b799
Maksym Yarema <macsym2@gmail.com>
2023-09-17 09:33:36 +0300

    complete C_GCD find gcd of multiple numbers task

---
+++ src/main/java/org/xsakon/eolymp/C_GCD.java
@@ -0,0 +1,38 @@
+package org.xsakon.eolymp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class C_GCD {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(in.readLine());
+        int[] arr = new int[size];
+
+        String[] s = in.readLine().split(" ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = Integer.parseInt(s[i]);
+        }
+
+        System.out.println(gcdOfMultipleNumbers(arr));
+        in.close();
+    }
+
+    static int gcdOfMultipleNumbers(int[] arr) {
+        int gcd = arr[0];
+
+        for (int n : arr) {
+            gcd = gcd(gcd, n);
+        }
+        return gcd;
+    }
+
+    static int gcd(int a, int b) {
+        while (a != 0 && b != 0) {
+            if (a > b) a %= b;
+            else b %= a;
+        }
+        return a + b;
+    }
+}
