a781409f046d589e99d47928cb12a021acdaa68e
Maksym Yarema <macsym2@gmail.com>
2023-09-17 10:03:38 +0300

    Complete E task

---
+++ src/main/java/org/xsakon/eolymp/E_RooksOnChessBoard.java
@@ -0,0 +1,21 @@
+package org.xsakon.eolymp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class E_RooksOnChessBoard {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger n = BigInteger.valueOf(Long.parseLong(in.readLine()));
+
+        System.out.println(factorial(n));
+        in.close();
+    }
+
+    public static BigInteger factorial (BigInteger n) {
+        if (n.equals(BigInteger.ZERO)) return BigInteger.ONE;
+        else return n.multiply(factorial(n.subtract(BigInteger.ONE)));
+    }
+}
