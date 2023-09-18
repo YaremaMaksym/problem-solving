b29a3da0edbf26d10ab1693a59e754526dddf436
Maksym Yarema <macsym2@gmail.com>
2023-09-18 10:08:51 +0300

    Complete J task

---
+++ src/main/java/org/xsakon/eolymp/J_LakeWithLilies.java
@@ -0,0 +1,13 @@
+package org.xsakon.eolymp;
+
+import java.util.Scanner;
+
+public class J_LakeWithLilies {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        System.out.println(n - 1);
+    }
+}
