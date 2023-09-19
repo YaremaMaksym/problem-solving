60abbab156b221b9811f4b1c34c3ffbdca92826f
Maksym Yarema <macsym2@gmail.com>
2023-09-19 10:52:29 +0300

    Complete Q task - Baghdad passport

---
+++ src/main/java/org/xsakon/eolymp/Q_BaghdadPassport.java
@@ -0,0 +1,22 @@
+package org.xsakon.eolymp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//not valid 111, 110, 011, 000
+//valid 001, 010, 100, 101
+
+public class Q_BaghdadPassport {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
+
+        int min = (n % 3 == 0) ? n / 3 : n / 3 + 1;
+        int max = n / 2;
+
+        System.out.println(min + " " + max);
+
+        in.close();
+    }
+}
