2fcbba6bff844ef969d7ed8a7744cdbc8aacdfbc
Maksym Yarema <macsym2@gmail.com>
2023-09-19 14:36:47 +0300

    Complete R and T tasks

---
+++ src/main/java/org/xsakon/eolymp/R_StephensAlarms.java
@@ -0,0 +1,30 @@
+package org.xsakon.eolymp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class R_StephensAlarms {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = in.readLine().split(" ");
+
+        int m = Integer.parseInt(s[0]);
+        int n = Integer.parseInt(s[1]);
+
+        System.out.println(lcm(m, n));
+        in.close();
+    }
+
+    static long lcm(long a, long b) {
+        return a * b / gcd(a, b);
+    }
+
+    static long gcd(long a, long b) {
+        while (a != 0 && b != 0) {
+            if (a > b) a = a % b;
+            else b = b % a;
+        }
+        return a + b;
+    }
+}
---
+++ src/main/java/org/xsakon/eolymp/T_StephenAndGum.java
@@ -0,0 +1,30 @@
+package org.xsakon.eolymp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class T_StephenAndGum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = in.readLine().split(" ");
+
+        int t1 = Integer.parseInt(s[0]);
+        int t2 = Integer.parseInt(s[1]);
+
+        System.out.println(lcm(t1, t2));
+        in.close();
+    }
+    
+    static long lcm(long a, long b) {
+        return a * b / gcd(a, b);
+    }
+
+    static long gcd(long a, long b) {
+        while (a != 0 && b != 0) {
+            if (a > b) a = a % b;
+            else b = b % a;
+        }
+        return a + b;
+    }
+}
