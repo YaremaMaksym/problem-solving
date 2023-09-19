7fb3bd853721493c1855784e07aff27c20fdea88
Maksym Yarema <macsym2@gmail.com>
2023-09-19 09:24:08 +0300

    Complete P task - Divisibility by 3

---
+++ src/main/java/org/xsakon/eolymp/P_DivisibilityByThree.java
@@ -0,0 +1,18 @@
+package org.xsakon.eolymp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P_DivisibilityByThree {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
+
+        int count = n / 3 * 2;
+        count += (n % 3 > 1) ? n % 3 - 1 : 0;
+
+        System.out.println(count);
+        in.close();
+    }
+}
