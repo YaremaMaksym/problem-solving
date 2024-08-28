7c7a239a47210f757ccedeebd120ec6231142072
Maksym Yarema <macsym2@gmail.com>
2024-08-28 17:03:41 +0300

    leetcode: 49. Group Anagrams

---
+++ src/main/java/org/xsakon/leetcode/p49GroupAnagrams.java
@@ -0,0 +1,29 @@
+package org.xsakon.leetcode;
+
+import java.util.*;
+
+public class p49GroupAnagrams {
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+
+        Map<String, List<String>> map = new HashMap();
+
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+
+            map.get(sortedWord).add(word);
+        }
+        return map.values().stream().toList();
+    }
+}
